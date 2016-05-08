66a4c28cb515d9cf4e62d6f913fe404addba8f19
Mike Gysel <mikegysel@gmail.com>
2016-05-08 15:09:13 -0700

    pizza website update

---
+++ views/Gruntfile.js
@@ -0,0 +1,78 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    /* Changes image sizes */
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            /* Change these */
+            width: 60,
+            suffix: '_small',
+            quality: 30
+          },
+          {
+            /* Change these */
+            width: 300,
+            suffix: '_large',
+            quality: 30
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+
+    /* image minification */
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'images/'
+            }]
+        }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/main.js',
+        dest: 'js/main.min.js'
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['responsive_images', 'imagemin', 'uglify', 'cssmin']);
+};
