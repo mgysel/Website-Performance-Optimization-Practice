d323ce7949e569e0607f5b02c84f4c01c618fd39
Mike Gysel <mikegysel@gmail.com>
2016-05-08 11:59:40 -0700

    Resized image files

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    /* Changes image sizes */
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            /* Change these */
+            width: 120,
+            suffix: '_small',
+            quality: 30
+          },
+          {
+            /* Change these */
+            width: 605,
+            suffix: '_large',
+            quality: 30
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'img/'
+        }]
+      }
+    },
+
+
+    /* image minification */
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'img/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'img/'
+            }]
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['responsive_images', 'imagemin']);
+};
