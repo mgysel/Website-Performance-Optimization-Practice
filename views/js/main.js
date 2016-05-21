d2b9e3a2eaf48478987e857c7ab2961cf22ec8a8
Mike Gysel <mikegysel@gmail.com>
2016-05-21 12:38:14 -0700

    Reduced number of pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -489,7 +489,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
   var topScroll = document.body.scrollTop/1250;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((topScroll) + (i % 5));
@@ -513,7 +513,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 35; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza-60_small.png";
@@ -521,7 +521,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    document.getElementById("movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
